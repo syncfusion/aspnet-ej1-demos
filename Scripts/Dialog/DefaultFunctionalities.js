03768940ddec608210b04cce5a4c628a475c7df4
BuildAutomation <buildautomation@syncfusion.com>
2021-09-30 14:36:51 +0530

    v19.3.0.43

--- Scripts/Dialog/DefaultFunctionalities.js
+++
@@ -1,9 +0,0 @@
-﻿
-function onDialogClose(args) {
-    $("#LayoutSection_ControlsSection_btnOpen").show();
-}
-function btnclick() {
-    var dialogObj = $("#LayoutSection_ControlsSection_basicDialog").data("ejDialog");
-    dialogObj.open();
-    $("#LayoutSection_ControlsSection_btnOpen").hide();
-}
